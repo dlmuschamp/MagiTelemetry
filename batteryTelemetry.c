34d78fb38a35967278df9f9d08cb52575f16534a
Damian Luciano Muschamp <muschampdamian@gmail.com>
2026-06-15 01:37:30 -0700

    Added comments explaining the C syntax differences.

--- batteryTelemetry.c
+++ batteryTelemetry.c
@@ -12,27 +12,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//define is basically acting as a global with text replacement 
+//#define is a "preprocesser directive" so its basically direct text replacement
+//during compilation. we use this for declaring arrays in C because array declaration 
+//requires a specific numerical value to know how much space to allocate for that array.
+
 #define BAT_PERCENT_PATH "/sys/class/power_supply/BAT0/capacity"
 #define BAT_BUFFER_SIZE 10
 
 int main() {
 
   FILE *bat_percent_info = fopen(BAT_PERCENT_PATH, "r");
-  
+
   if (bat_percent_info == NULL) {
     printf("Could not locate a file at the specified path.\n");
     return 1;
-  }  
+  }
 
+  //New char buffer (string) to store the battery percent info
   char perc_buffer[BAT_BUFFER_SIZE];
 
+  //Reading the file and writing the characters to the buffer
   fgets(perc_buffer, BAT_BUFFER_SIZE, bat_percent_info);
 
+  //Printing the final buffer. %s means to print the entire string (char buffer)
   printf("Current battery: %s", perc_buffer);
 
   fclose(bat_percent_info);
-
 
   //Successful Exit
   return 0; 
